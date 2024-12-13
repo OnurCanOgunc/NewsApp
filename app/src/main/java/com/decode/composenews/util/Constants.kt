fd7f67de16ce6226124deae136555d82ceec5364
Onur Can Öğünç <ogunc606@gmail.com>
2024-12-13 13:10:30 +0300

    Retrieve API key from BuildConfig

--- app/src/main/java/com/decode/composenews/util/Constants.kt
+++ app/src/main/java/com/decode/composenews/util/Constants.kt
@@ -1,13 +1,15 @@
 package com.decode.composenews.util
 
+import com.decode.composenews.BuildConfig
+
 object Constants {
     const val BASE_URL = "https://api.currentsapi.services/v1/"
     const val END_POINT_LATEST_NEWS = "latest-news"
     const val END_POINT_SEARCH = "search"
-    const val LANGUAGE = "en"
+    const val LANGUAGE = "tr"
     const val PAGE_SIZE = 20
     const val INITIAL_LOAD_SIZE = 30
-    const val API_KEY = ""
+    const val API_KEY = BuildConfig.API_KEY
 }
 
 
