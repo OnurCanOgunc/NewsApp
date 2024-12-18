35a1e24e34d3a46a29514f7508febb2b0b5f59ee
Onur Can Öğünç <ogunc606@gmail.com>
2024-12-18 15:23:14 +0300

    Refactor formatDate to include date formatting

--- app/src/main/java/com/decode/composenews/util/CleanData.kt
+++ app/src/main/java/com/decode/composenews/util/CleanData.kt
@@ -1,5 +1,18 @@
 package com.decode.composenews.util
 
-fun String.cleanDate(): String {
-    return this.substringBeforeLast(" +")
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+
+fun String.formatDate(): String {
+    return try {
+        val inputFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss Z", Locale.ENGLISH)
+        val date = inputFormat.parse(this)
+
+        val outputFormat = SimpleDateFormat("dd MMMM yyyy HH:mm", Locale.ENGLISH)
+        outputFormat.format(date!!)
+    } catch (e: Exception) {
+        e.printStackTrace()
+        ""
+    }
 }
